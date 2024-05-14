dc9e3886bd61b82ef33395d53b3a8a0048f74fc3
sofiacris <161314790+sofisisi@users.noreply.github.com>
2024-05-14 10:27:48 -0300

    Update atvd13.js

--- atvd13.js
+++ atvd13.js
@@ -2,23 +2,24 @@ function calcular() {
     let var1 = document.getElementById("var1").value; 
     let var2 = document.getElementById("var2").value;
     
-    let nomeFuncionario = document.getElementById("nomeFuncionario").value
-    let salarioBruto = document.getElementById("salarioBruto").valueS
+    let nomeFuncionario = document.getElementById("nomeFuncionario").value;
+    let salarioBruto = document.getElementById("salarioBruto").value;
+    let taxaDesconto; 
 
-    let resultado = ""
+    let resultado = ""; 
 
     if (var2 < var1) {
         resultado += "Diferença entre duas variavéis:" + (var1 - var2) + "<br>";
     } else {
         resultado += "Diferença entre duas variavéis:" + (var2 - var1) + "<br>";
     }
-    resultado += "O dobro da primeira mais o triplo da segunda é:" + (2* var1 + 3* var2) + "<br>";
+    resultado += "O dobro da primeira mais o triplo da segunda é:" + (2*var1 + 3*var2) + "<br>";
     resultado += "Multiplicação das duas variáveis é:" + (var1 * var2 ) + "<br>"
 
     if (var2 < var1) {
-        resultado += "Em ordem decrescente:" + var1 + "," + var2 + "<br>"; 
+        resultado += "Em ordem decrescente:" + var2 + "," + var1 + "<br>"; 
     } else {
-        resultado += "Em ordem decrescente:" + var2 + "," + var1 + "<br>";  
+        resultado += "Em ordem decrescente:" + var1 + "," + var2 + "<br>";  
     }
 
     if (salarioBruto <= 1000){
